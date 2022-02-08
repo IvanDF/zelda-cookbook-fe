92fb4eccbc85c11e4c7ea5e4cc0091415f7ca703
IvanDF <delfatti.ivan@gmail.com>
2022-02-08 19:11:38 +0100

    ADD: create and init Global Theme object

---
+++ src/costants/Theme.ts
@@ -0,0 +1,49 @@
+export const Theme = {
+  color: {
+    // Basic Colors
+    black: "#000000",
+    white: "#FFFFFF",
+    // Basic Colors
+    dark: "#333333",
+    light: "#DDDDDD",
+
+    active: "#E5FCFC", // Active color
+  },
+
+  // Font settings
+  font: {
+    //  Sizes
+    size: {
+      s14: "14px",
+      s18: "18px",
+      s24: "24px",
+    },
+
+    // Weight
+    weight: {
+      regular: 400,
+      bold: 700,
+    },
+  },
+
+  // Opacity
+  opacity: {
+    hide: 0,
+    o1: 0.1,
+    o2: 0.2,
+    o3: 0.3,
+    o4: 0.4,
+    o5: 0.5,
+    o6: 0.6,
+    o7: 0.7,
+    o8: 0.8,
+    o9: 0.9,
+  },
+};
+
+export const Effects = {
+  // Effects
+  blur: {},
+  shadow: {},
+  transition: {},
+};
