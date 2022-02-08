2b43f663f502074f1d70751ec5f286d7a54a72bb
IvanDF <delfatti.ivan@gmail.com>
2022-02-08 19:03:48 +0100

    FIX: clean & improve useFetch hook

--- src/feature/provaFetch/FetchDemo.tsx
+++ src/feature/provaFetch/FetchDemo.tsx
@@ -23,7 +23,7 @@ const FetchDemo: React.FC = () => {
           requestApiHandler({
             url: "recipe",
             method: ApiMethod.ADD,
-            body: {
+            payload: {
               name: "Nuova ricetta da React",
               description: "Nuova descrizione della ricetta da React",
               stat_id: 4,
@@ -40,7 +40,7 @@ const FetchDemo: React.FC = () => {
             url: "recipe",
             urlParams: [1],
             method: ApiMethod.UPDATE,
-            body: {
+            payload: {
               name: "Nuova ricetta aggiornata da React",
               description:
                 "Nuova descrizione della ricetta aggiornata da React",
