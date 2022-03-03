2c8fa2806ff54132a1f56c2986b14579c7f68d50
IvanDF <delfatti.ivan@gmail.com>
2022-03-03 22:23:25 +0100

    WIP: detail wrapper component

--- src/feature/sections/bag/Bag.tsx
+++ src/feature/sections/bag/Bag.tsx
@@ -1,15 +1,22 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import SliderBox from "../../../components/sliderBox/SliderBox";
 import { Icon } from "../../../components/ui/icon/Icon";
 import { IconType } from "../../../components/ui/icon/IconType";
-import useOutsideClickRef from "../../../hooks/useOutsideClick";
+import { TextType } from "../../../components/ui/text/IText";
+import Text from "../../../components/ui/text/Text";
 import useWindowSize from "../../../hooks/useWindowSize";
 import {
   BagMenu,
   BagSlider,
   BagWrapper,
+  BoxWrapper,
+  BoxWrapperBorder,
+  DescriptionWrapper,
   DetailWrapperDesktop,
   DetailWrapperMobile,
+  Heart,
+  Img,
+  ImgWrapper,
   Slider,
   Wrapper,
 } from "./BagStyles";
@@ -27,7 +34,7 @@ const Bag: React.FC = () => {
         <BagMenu>Menu</BagMenu>
         <BagSlider>
           <Icon name={IconType.PREV_ICON} width={64} height={64} />
-          <Slider>
+          <Slider device={breakpoint}>
             {list.map((el: number, i: number) => {
               return (
                 <SliderBox
@@ -44,7 +51,27 @@ const Bag: React.FC = () => {
         </BagSlider>
       </BagWrapper>
       {breakpoint === "DESKTOP" ? (
-        <DetailWrapperDesktop>Detail</DetailWrapperDesktop>
+        <DetailWrapperDesktop>
+          <ImgWrapper>
+            <Img />
+          </ImgWrapper>
+          <BoxWrapperBorder>
+            <BoxWrapper>
+              <DescriptionWrapper>
+                <Text textType={TextType.HEADING} text="Mela" />
+                <Heart />
+                <Heart />
+                <Heart />
+                <Heart />
+                <Heart />
+                <Text
+                  textType={TextType.TEXT}
+                  text="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquam minus porro magni debitis nam, perferendis, voluptatibus in optio odio ab tempora. Porro iste labore placeat in quaerat, reiciendis incidunt? Perferendis?"
+                />
+              </DescriptionWrapper>
+            </BoxWrapper>
+          </BoxWrapperBorder>
+        </DetailWrapperDesktop>
       ) : (
         <DetailWrapperMobile>Detail</DetailWrapperMobile>
       )}
--- src/feature/sections/bag/BagStyles.ts
+++ src/feature/sections/bag/BagStyles.ts
@@ -1,7 +1,11 @@
 import styled from "styled-components";
+import { Theme, ThemeExtra } from "../../../costants/Theme";
 import { IBreakpoint } from "../../../hooks/useWindowSize";
-import { DFlex, Fill } from "../../../styles/globalStyle/GlobalStyle";
-import { IDFlex } from "../../../styles/globalStyle/IGlobalStyle";
+import { DFlex, Fill, Position } from "../../../styles/globalStyle/GlobalStyle";
+import {
+  IDFlex,
+  PositionTypePosEnum,
+} from "../../../styles/globalStyle/IGlobalStyle";
 
 export const Wrapper = styled.div`
   ${DFlex(IDFlex.CENTER)}
@@ -18,10 +22,72 @@ export const BagWrapper = styled.div<{ device: IBreakpoint }>`
 export const DetailWrapperDesktop = styled.div`
   ${DFlex(IDFlex.RIGHT)}
   flex-direction: column;
-  height: 100%;
   width: 30%;
-  padding: 20px;
 `;
+
+export const BoxWrapperBorder = styled.div`
+  align-self: flex-end;
+  padding: 2px;
+  margin-bottom: 100px;
+  border-width: 2px;
+  border-style: solid;
+  border-image-source: ${ThemeExtra.background.gradient.darkToNone};
+  border-image-slice: 1;
+`;
+
+export const BoxWrapper = styled.div`
+  ${Position(PositionTypePosEnum.RL)}
+  ${DFlex(IDFlex.RIGHT)}
+  flex-direction: column;
+  width: 100%;
+  padding: 20px;
+  background: ${ThemeExtra.background.gradient.darkToNone};
+  backdrop-filter: ${ThemeExtra.blur.b10};
+`;
+
+export const ImgWrapper = styled.div`
+  width: 100%;
+  height: 600px;
+  background: #000;
+`;
+
+export const Img = styled.img`
+  display: block;
+  height: auto;
+  width: 100%;
+`;
+
+export const Heart = styled.span`
+  ${Position(PositionTypePosEnum.RL)}
+  display: inline-block;
+  margin: 20px 10px 10px 7px;
+  height: 15px;
+  width: 15px;
+  background: ${Theme.color.red};
+  transform: rotate(45deg);
+  &::after {
+    content: "";
+    ${Position(PositionTypePosEnum.AB)}
+    left: -10px;
+    top: 0px;
+    width: 20px;
+    height: 15px;
+    background: ${Theme.color.red};
+    border-radius: 50%;
+  }
+  &::before {
+    content: "";
+    ${Position(PositionTypePosEnum.AB)}
+    left: 0px;
+    bottom: 5px;
+    width: 15px;
+    height: 20px;
+    background: ${Theme.color.red};
+    border-radius: 50%;
+  }
+`;
+
+export const DescriptionWrapper = styled.div``;
 
 export const DetailWrapperMobile = styled.div`
   position: absolute;
@@ -37,13 +103,13 @@ export const BagMenu = styled.div`
 export const BagSlider = styled.div`
   ${DFlex(IDFlex.CENTER)}
   width: 100%;
-
   padding: 10px;
 `;
 
-export const Slider = styled.div`
+export const Slider = styled.div<{ device: IBreakpoint }>`
   display: grid;
-  grid-template-columns: repeat(5, 1fr);
+  grid-template-columns: ${(p) =>
+    p.device === "DESKTOP" ? "repeat(5, 1fr)" : "repeat(3, 1fr)"};
   grid-template-rows: repeat(3, 1fr);
   align-items: center;
   justify-content: center;
